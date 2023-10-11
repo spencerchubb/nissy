6316bbe24b5d72620288ba0facf7c9d47657cbc0
spencerchubb <spencerchubb@gmail.com>
2023-10-10 22:51:32 -0400

    update

--- app/main.py
+++ app/main.py
@@ -54,4 +54,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0', port=80)
--- build.sh
+++ build.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+# Install dependencies
+sudo apt update
+sudo apt install python3-pip gcc
+sudo pip3 install -r requirements.txt
+
 # Compile the cli
 # gcc src/*.c -o nissy -DVERSION=\"2.0.6\"
 
+cd app
+
 # Compile the shared library for the python wrapper
-gcc -shared -o nissy.so -fPIC src/*.c -DVERSION=\"2.0.6\"
-python3 app/main.py
+gcc -shared -o nissy.so -fPIC ../src/*.c -DVERSION=\"2.0.6\"
+
+# Running with sudo is needed to access port 80
+sudo python3 app/main.py
+
+# Deploy in prod
+sudo kill -9 $(sudo lsof -t -i:80)
+nohup sudo waitress-serve --port=80 main:app > log.txt 2>&1 &
+cat log.txt
